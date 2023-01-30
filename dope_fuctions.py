ceec49dc4ea467f1d6a30e6b35dee04755fc3a51
Heramb Skanda <43760635+herambskanda@users.noreply.github.com>
2023-01-30 22:36:07 +0530

    Update dope_fuctions.py

--- dope_fuctions.py
+++ dope_fuctions.py
@@ -1,12 +1,27 @@
-def fun(lim, s = []):
-    i = 0
-    s += [i]
-    lim -= 1
-    if i == lim:
-        return(s)
-    else:
-        fun(lim, s)
+def unique_factors(root_number, fac_list, prefix:list = []): # except itself
+    for i in range(2,root_number):
+        if root_number%i == 0:
+            prefix += [i]
+            x = prefix+[int(root_number/i)]
+            x.sort(reverse=True)
+            duplicate = False
+            for y in fac_list:
+                if y == x: 
+                    duplicate = True
+            if not duplicate:
+                fac_list.append(x)
+            unique_factors(int(root_number/i), fac_list, prefix )
+    if len(prefix) > 0:
+        print("---")
+        prefix.pop(-1)
 
-s = []
-fun(5,s)
-print(s)
+def all_factors(root_number, fac_list, prefix:list = []):#except itself
+    for i in range(2,root_number):
+        if root_number%i == 0:
+            prefix += [i]
+            x = prefix+[int(root_number/i)]
+            fac_list.append(x)
+            all_factors(int(root_number/i), fac_list, prefix )
+    if len(prefix) > 0:
+        print("---")
+        prefix.pop(-1)
